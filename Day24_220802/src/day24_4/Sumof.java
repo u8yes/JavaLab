2787793529329fbcb024520689d8a7b94361b5c2
u8yes <108450018+u8yes@users.noreply.github.com>
2022-08-03 10:48:42 +0900

    Day25 20220803

--- Day24_220802/src/day24_4/Sumof.java
+++ Day24_220802/src/day24_4/Sumof.java
@@ -13,7 +13,7 @@ public class Sumof {
 		char[] cNum = sNum.toCharArray();
 		int sum = 0;
 		for(int i = 0; i < cNum.length; i++) {
-			sum += cNum[i] - 0;
+			sum += cNum[i] - 0;	// cNum[i]를 정수형으로 변환하면서 sum에 더하여 누적하기
 		}
 		System.out.println(sum);
 	}
